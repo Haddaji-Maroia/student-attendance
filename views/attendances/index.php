9f7bb3f0ee26b65105838865850fe9c278365c97
Haddaji-Maroia <maroia.haddaji@student.hepl.be>
2026-03-01 15:37:04 +0100

    fix: broken paths
    Les chemin cassées ont été réparés et on a profité pour généraliser les paths les plus essentiels

--- views/attendances/index.php
+++ views/attendances/index.php
@@ -1,28 +1,28 @@
-<?php include VIEWS_DIR.'/partials/head.php' ?>
+<?php include VIEWS_PATH . '/partials/head.php' ?>
 <?php global $title ?>
-<main class="page-main">
-    <h1><?php echo $title ?></h1>
-    <form action="" method="POST">
-        <ol class="student-list">
-            <?php foreach (getAllStudents() as $student) { ?>
-                <li>
-                    <input id="<?php echo $student['id'] ?>" type="checkbox" name="students[]"
-                           value="<?php echo $student['id'] ?>">
-                    <label for="<?php echo $student['id'] ?>"><?php echo $student['first_name'] ?>
-                        &nbsp;<?php echo $student['last_name'] ?></label>
-                </li>
-            <?php } ?>
-        </ol>
+    <main class="page-main">
+        <h1><?php echo $title ?></h1>
+        <form action="" method="POST">
+            <ol class="student-list">
+                <?php foreach (getAllStudents() as $student): ?>
+                    <li>
+                        <input id="<?php echo $student['id'] ?>" type="checkbox" name="students[]"
+                               value="<?php echo $student['id'] ?>">
+                        <label for="<?php echo $student['id'] ?>"><?php echo $student['first_name'] ?>
+                            &nbsp;<?php echo $student['last_name'] ?></label>
+                    </li>
+                <?php endforeach; ?>
+            </ol>
 
-        <button type="submit">Enregistrer les présences</button>
-    </form>
-    <button class="randomStudentBtn hidden">Choisir un·e étudiant·e</button>
-    <p class="currentStudent hidden"></p>
-</main>
+            <button type="submit">Enregistrer les présences</button>
+        </form>
+        <button class="randomStudentBtn hidden">Choisir un·e étudiant·e</button>
+        <p class="currentStudent hidden"></p>
+    </main>
 
 
-<?php include VIEWS_DIR.'/partials/nav.php' ?>
+<?php include VIEWS_PATH . '/partials/nav.php' ?>
 
-<script defer src="/assets/js/main.js"></script>
+    <script defer src="/assets/js/main.js"></script>
 
-<?php include VIEWS_DIR.'/partials/footer.php' ?>
+<?php include VIEWS_PATH . '/partials/footer.php' ?>
--- views/students/index.php
+++ views/students/index.php
@@ -1,27 +1,27 @@
-<?php include VIEWS_DIR.'/partials/head.php' ?>
+<?php include VIEWS_PATH . '/partials/head.php' ?>
 
 <?php global $title ?>
-<main class="page-main">
-    <h1><?php echo $title ?></h1>
-    <?php $students = getAllStudents(); ?>
+    <main class="page-main">
+        <h1><?php echo $title ?></h1>
+        <?php $students = getAllStudents(); ?>
 
-    <?php if (count($students) > 0) { ?>
-        <ol>
-            <?php foreach (getAllStudents() as $student) { ?>
-                <li>
-                    <?php echo $student['first_name'] ?>
-                    &nbsp;<?php echo $student['last_name'] ?>
-                </li>
-            <?php } ?>
-        </ol>
-    <?php } else { ?>
-        <p>Mais où sont-ils&nbsp;?</p>
-    <?php } ?>
+        <?php if (count($students) > 0): ?>
+            <ol>
+                <?php foreach (getAllStudents() as $student): ?>
+                    <li>
+                        <?php echo $student['first_name'] ?>
+                        &nbsp;<?php echo $student['last_name'] ?>
+                    </li>
+                <?php endforeach; ?>
+            </ol>
+        <?php else: ?>
+            <p>Mais où sont-ils&nbsp;?</p>
+        <?php endif; ?>
 
-</main>
-
-
-<?php include VIEWS_DIR.'/partials/nav.php' ?>
+    </main>
 
 
-<?php include VIEWS_DIR.'/partials/footer.php' ?>
+<?php include VIEWS_PATH . '/partials/nav.php' ?>
+
+
+<?php include VIEWS_PATH . '/partials/footer.php' ?>

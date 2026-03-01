a183a4775c4a97a5fccd035090d30bf7f3668f6a
Haddaji-Maroia <maroia.haddaji@student.hepl.be>
2026-03-01 14:32:15 +0100

    fix: make tables available to php scripts
    Le code de connection qui permet de réaliser les migrations, en plus du fonctionnement normal du site, a été mis à jour pour lui charger lui aussi les données de l'environnement

--- db/connexion.php
+++ db/connexion.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once '../vendor/autoload.php';
+require_once __DIR__. '/../vendor/autoload.php';
 
 if (!isset($dotenv)){
-    $dotenv = Dotenv\Dotenv::createImmutable(APP_PATH);
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__. '/../');
     $dotenv->load();
 }
 
